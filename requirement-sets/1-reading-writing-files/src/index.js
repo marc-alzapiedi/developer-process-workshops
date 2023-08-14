f1c8516475017bd7c4a417c21baecc767abab77e
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-08-14 21:42:42 +0200

    some work

--- requirement-sets/1-reading-writing-files/src/index.js
+++ requirement-sets/1-reading-writing-files/src/index.js
@@ -1,1 +1,25 @@
 // run the program
+
+const fs = require('fs')
+const FileSaver = require('file-saver')
+const Blob = require('buffer')
+
+function createVariables (input) {
+  let count = 0
+  const allLines = input.split(/\r\n|\n/)
+  console.log(allLines)
+  allLines.forEach((line) => {
+    if (line.length === 1 || line.charAt(0) === line.charAt(line.length - 1)) {
+      count++
+    }
+  })
+  return count
+}
+
+fs.readFile('C:/Users/marca/Desktop/input.txt', (err, data) => {
+  if (err) throw err
+  const input = data.toString()
+  createVariables(input)
+  const blob = new Blob([`count: ${createVariables(input)}`], { type: 'text/plain;charset=utf-8' })
+  FileSaver.saveAs(blob, 'testfile1.txt')
+})
